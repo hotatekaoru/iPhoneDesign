28e59ba0e5601b57a8cf029a29295c05f736c26b
Hotate <kaorupochi@gmail.com>
2016-10-21 23:53:39 +0900

    initial commit

---
+++ iPhoneDesign/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPhoneDesign
+//
+//  Created by 保立馨 on 2016/10/21.
+//  Copyright © 2016年 Kaoru Hotate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
